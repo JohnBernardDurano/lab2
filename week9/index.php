077b0a41c39c343fc842362d91db293c2bbbf466
John Bernard Durano <jsdurano@student.apc.edu.ph>
2023-03-05 00:54:20 +0800

    Added Week 9 Files

---
+++ week9/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width", initial-scale="1.0">
+    <meta charset="UTF-8">
+    <title>FFHooray</title>
+    <link rel="icon" type="image/x-icon" href="../images/FFHooray.png">
+    <link rel="stylesheet" href="css/index_style.css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+  </head>
+  <body class="bodyStyle">
+
+  <!-- This will become the most cringe webpage that will be passed for this class, BELIEVE IT -->
+
+  <header>
+    <div class="navigation">
+      <a href="index.html">Home</a>|
+      <a href="#">The Badudong</a>|
+      <a href="resources.html">Resources</a>
+  </header>
+
+  <section>
+    
+    <div style="overflow:auto">
+      <div>
+      </div>
+    
+      <article class="main">
+        <h2>FFH</h2>
+        <p>FFH or FFHooray is a Discord Server made by League of Legends players who wanted a place where they can hang out and play games together. 
+          The image on the right is the mascot of the server called <q>The Badudong</q> and he is always watching.
+          Come and hang out with us here in the Discord Server by clicking on the button that says join now, if you want to know more about the author of this page,
+          click on the button that says click here. Click on the 3rd button to get to the Javascript Testing Grounds.
+        </p>
+        <a href="https://discord.gg/7kfUPRsAZH" class="discord-btn">JOIN NOW</a>
+        <a href="#" class="author-btn">CLICK HERE</a>
+      </article>
+    
+      <div class="badudong_img">
+        <img src="images/badudong.png" alt="The Badudong" >
+      </div>
+    </div> 
+  </section>
+
+  <footer>
+    <p>&quot;You are now under the Badudong that is always watching.&quot;</p>
+  </footer>
+
+  </body>
+</html>
