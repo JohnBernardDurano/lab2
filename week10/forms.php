d623df950e54c6095a4644f40b1ffe24a9acf1a1
John Bernard Durano <jsdurano@student.apc.edu.ph>
2023-03-05 22:31:12 +0800

    commit

--- week10/forms.php
+++ week10/forms.php
@@ -21,92 +21,14 @@
 <section>
 <?php
 
-$gstname = $email = $gender = $messages = $website = "";
-$gstnameErr = $emailErr = $genderErr = $messagesErr = $websiteErr = $vtuberErr = "";
+$name = $email = $gender = $messages = $website = $vtuber = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["gstname"])) {
-    $gstnameErr = "Name is required";
-  } else {
-    $gstname = test_input($_POST["gstname"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$gstname)) {
-      $gstnameErr = "Only letters and white space allowed";
-    }
-  }
-}
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
-    }
-  }
-    
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL";
-    }
-  }
-
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-
-if (empty($_POST["name"])) {
-  $nameErr = "Name is required";
-} else {
-  $name = test_input($_POST["lastname"]);
-  // check if lastname only contains letters and whitespace
-  if (!preg_match("/^[a-zA-Z-' ]*$/",$lastname)) {
-    $lastnameErr = "Only letters and white space allowed";
-  }
-}
-
-if (empty($_POST["email"])) {
-  $emailErr = "Email is required";
-} else {
+  $name = test_input($_POST["name"]);
   $email = test_input($_POST["email"]);
-  // check if e-mail address is well-formed
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $emailErr = "Invalid email format";
-  }
-}
-  
-if (empty($_POST["website"])) {
-  $website = "";
-} else {
   $website = test_input($_POST["website"]);
-  // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-  if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-    $websiteErr = "Invalid URL";
-  }
-}
-
-if (empty($_POST["gender"])) {
-  $genderErr = "Selecting something is required";
-} else {
+  $vtuber = test_input($_POST["vtuber"]);
   $gender = test_input($_POST["gender"]);
-}
-
-if (empty($_POST["comment"])) {
-  $comment = "";
-} else {
-  $comment = test_input($_POST["comment"]);
 }
 
 function test_input($data) {
@@ -116,14 +38,17 @@ function test_input($data) {
   return $data;
 }
 ?>
-<h2>Input your details.</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" style="text-align: right; color:aliceblue;">  
-  Name: <input type="text" name="gstname">
+
+<h2>Input your details.</h2>;
+
+<form method="post";action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" style="color: aliceblue; text-align:right">  
+  Name: <input type="text" name="name">
   <br><br>
   E-mail: <input type="text" name="email">
   <br><br>
   Website: <input type="text" name="website">
   <br><br>
+  Favorite Vtuber: <input type="text" name="vtuber"><br><br>
   Gender:
   <input type="radio" name="gender" value="male">Male
   <input type="radio" name="gender" value="female">Female
@@ -136,26 +61,24 @@ function test_input($data) {
 
 <?php
 echo "<h2>Your Input:</h2>";
-echo "<div style=\"color:aliceblue;\">" . $gstname . "</div>";
+echo "<div style=\"color:aliceblue;\">" . $name . "</div>";
 echo "<br>";
 echo "<div style=\"color:aliceblue;\">" . $email . "</div>";
 echo "<br>";
 echo "<div style=\"color:aliceblue;\">" . $website . "</div>";
 echo "<br>";
+echo "<div style=\"color:aliceblue;\">" . $vtuber . "</div>";
+echo "<br>";
 echo "<div style=\"color:aliceblue;\">" . $gender . "</div>";
 echo "<br>";
-echo "<div style=\"color:aliceblue;\">" . $messages . "</div>";
-?>
-
-<?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
 {
 
 	$servername = "localhost";
-	$username = "webprogmi212";
-	$password = "webprogmi212";
-	$dbname = "webprogmi212";
+	$username = "root";
+	$password = "";
+	$dbname = "guestdb";
 	
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
@@ -164,11 +87,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	die("Connection failed: " . $conn->connect_error);
 	}
 	
-	$sql = "INSERT INTO mnturingan_myallies (firstname, lastname, email)
-	VALUES ('$name', '$lastname', '$email')";
+	$sql = "INSERT INTO guests (gstname, email, website, vtuber, messages)
+	VALUES ('$name', '$email', '$website', '$vtuber', '$messages')";
 	
 	if ($conn->query($sql) === TRUE) {
-	echo "You just joined the alliance. There is no going back. Hehe";
+	echo "Thank you for voting for my website.";
 	} else {
 	echo "Error: " . $sql . "<br>" . $conn->error;
 	}
@@ -176,10 +99,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	$conn->close();
 }
 ?>
-
 </section>
-
-
 
     <footer>
         <p>&quot;You are now under the Badudong that is always watching.&quot;</p>
