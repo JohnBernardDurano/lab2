9b440b98a79b95c1161d4c26ce1c6fc86be26e92
John Bernard Durano <jsdurano@student.apc.edu.ph>
2023-03-05 16:14:08 +0800

    Made my first php Forms

---
+++ week9/welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+Welcome <?php echo $_POST["name"]; ?> <br>
+Your email address is: <?php echo $_POST["email"]; ?> <br>
+Your favorite Vtuber is: <?php echo $_POST["favVtuber"]; ?> <br>
+You identify as: <?php echo $_POST["gender"]; ?> <br>
+You commented: <?php echo $_POST["comment"]; ?>
+</body>
+</html>
